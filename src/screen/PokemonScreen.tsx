b235a442a563eae4518cc183bee612dd75757d69
andres-hub <andrescalderoncl@gmail.com>
2022-10-07 17:08:35 -0500

    ok  until the video 255

---
+++ src/screen/PokemonScreen.tsx
@@ -0,0 +1,18 @@
+import React from 'react'
+import { StackScreenProps } from '@react-navigation/stack';
+import { Text, View } from 'react-native';
+import { RootStackParams } from '../navigator/Navigator';
+
+interface Props extends StackScreenProps<RootStackParams, 'PokemonScreen'> { };
+
+export const PokemonScreen = ({navigation, route}: Props) => {
+
+    const { simplePokemon, color } = route.params;
+
+    return (
+        <View >
+            <Text>{ simplePokemon.name } - {color}</Text>
+            
+        </View>
+    )
+}
